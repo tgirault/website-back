dd5d44d01466e274998f43016cbdfffa995c6277
Thomas <t.girault@gmail.com>
2020-01-23 20:05:12 +0100

    Initialisation du projet

---
+++ src/test/java/fr/girault/thomas/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.girault.thomas.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
